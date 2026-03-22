d9231e644ba9ad72713a25fdc5387f37a2a7ecc4
Vaibhavi Sutar <vaibhavi7029@gmail.com>
2026-03-22 19:41:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,48 +2,24 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(name, price) {
     let item = cart.find(p => p.name === name);
-
     if (item) item.qty++;
     else cart.push({ name, price, qty: 1 });
-
     updateCart();
 }
 
 function updateCart() {
-    let cartDiv = document.getElementById("cartItems");
-    let total = 0;
-    cartDiv.innerHTML = "";
-
-    cart.forEach((item, index) => {
-        total += item.price * item.qty;
-
-        cartDiv.innerHTML += `
-        <div>
-            ${item.name} (${item.qty})
-            <button onclick="changeQty(${index}, -1)">-</button>
-            <button onclick="changeQty(${index}, 1)">+</button>
-            <button onclick="removeItem(${index})">❌</button>
-        </div>`;
-    });
-
-    document.getElementById("total").innerText = total;
+    document.getElementById("cartCount").innerText = cart.length;
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-function changeQty(index, change) {
-    cart[index].qty += change;
-    if (cart[index].qty <= 0) cart.splice(index, 1);
-    updateCart();
-}
+function searchProduct() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
 
-function removeItem(index) {
-    cart.splice(index, 1);
-    updateCart();
-}
-
-function goToPayment(){
-    if(cart.length === 0) alert("Cart is empty!");
-    else window.location = "payment.html";
+    for (let i = 0; i < cards.length; i++) {
+        let text = cards[i].innerText.toLowerCase();
+        cards[i].style.display = text.includes(input) ? "block" : "none";
+    }
 }
 
 updateCart();
