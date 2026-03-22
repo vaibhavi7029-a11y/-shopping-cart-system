d8bf4b0afc220dadefe8e15c2422774a74c63e4a
Vaibhavi Sutar <vaibhavi7029@gmail.com>
2026-03-22 14:42:15 +0530

    Update login.c

--- login.c
+++ login.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char username[20], password[20];
+
+    printf("Enter Username: ");
+    scanf("%s", username);
+
+    printf("Enter Password: ");
+    scanf("%s", password);
+
+    if(strcmp(username,"admin")==0 && strcmp(password,"1234")==0)
+        printf("Login Successful\n");
+    else
+        printf("Invalid Login\n");
+
+    return 0;
+}
